f12d7a5688d3618fdcc39c897d5ea2d4d1b6393b
gkswns04 <Hanjun@gimhanjun-ui-MacBook-Pro.local>
2017-03-29 16:57:03 +0900

    ADD(all)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import { observable, computed, action } from 'mobx';
-import { observer } from 'mobx-react';
+import { observable, computed, action, autorun } from 'mobx';
+import { observer, Provider } from 'mobx-react';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import DevTools from 'mobx-react-devtools';
 
-const t = new class Temperature {
+class Temperature {
+  id = Math.random();
   @observable unit = "C";
   @observable temperatureCelsius = 25;
+  @observable location = "Incheon";
+  @observable loading = true;
+  @observable APPID = "189cdd61d8cad90c75fec754216a3fbc";
+
+  constructor(location) {
+    this.location = location;
+    this.fetch();
+  }
+
+  @action fetch() {
+    window.fetch(`http://api.openweathermap.org/data/2.5/weather?appid=${this.APPID}&q=${this.location}`)
+      .then(res => res.json()
+        .then(json => {
+          this.temperatureCelsius = json.main.temp -273.15;
+          this.loading = false;
+        })
+      )
+  }
 
   @computed get temperatureKelvin() {
     return this.temperatureCelsius * (9/5) + 32
@@ -36,18 +55,106 @@ const t = new class Temperature {
     this.setCelsius(degrees);
     this.setUnit(unit);
   }
+
+  @action inc() {
+    this.setCelsius(this.temperatureCelsius + 1);
+  }
 }
 
-const App = observer(({ temperature }) => (
-  <div>
-    {temperature.temperature}
+const App = observer(
+  ["temperatures"],
+  ({ temperatures }) => (
+  <ul>
+    <TemperatureInput />
+    {temperatures.map(t =>
+      <TView key={t.id} temperature={t} />
+    )}
     <DevTools />
-  </div>
+  </ul>
 ))
 
+@observer(["temperatures"])
+class TemperatureInput extends Component {
+  @observable input = "";
+
+  render() {
+    return (
+      <li>
+        Destination:
+        <input onChange={this.onChange} value={this.input} />
+        <button onClick={this.onSubmit}>Add</button>
+      </li>
+    )
+  }
+
+  @action onChange = (e) => {
+    this.input = e.target.value
+  }
+
+  @action onSubmit = () => {
+    this.props.temperatures.push(
+      new Temperature(this.input)
+    )
+    this.input = ""
+  }
+}
+
+
+@observer class TView extends Component {
+  render() {
+    const t = this.props.temperature
+    return (
+      <li onClick={this.onTemperatureClick}
+      >
+        {t.location}:
+        {t.loading ? "loading..." : t.temperature}
+      </li>
+    );
+  }
+
+  @action onTemperatureClick = () => {
+    this.props.temperature.inc()
+  }
+}
+
+const temps = observable([])
+
 ReactDOM.render(
-  <App temperature={t} />,
+  <Provider temperatures={temps}>
+    <App />
+  </Provider>,
   document.querySelector('.container')
 )
 
-window.t = t;
+// function isNice(t) {
+//   return t.temperatureCelsius > 25
+// }
+
+// when(
+//   () => temps.some(isNice),
+//   () => {
+//     const t = temps.find(isNice)
+//     alert("Book now! " + t.location)
+//   }
+// )
+
+// function render(temperatures) {
+//   return `
+//     <ul>
+//       ${temperatures.map(t =>
+//         `<li>
+//           ${t.location}:
+//           ${t.loading ? "loading" : t.temperature}
+//         </li>`
+//       ).join("")}
+//     </ul>
+//   `
+// }
+//
+// temps.push(new Temperature("Amsterdam"))
+// temps.push(new Temperature("Rotterdam"))
+//
+// autorun (() => {
+//   document.querySelector('container').innerHTML =
+//     render(temps)
+// })
